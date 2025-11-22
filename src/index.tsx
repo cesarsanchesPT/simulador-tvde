f4011131fc939248efd5847271ac1124350447d2
Cesar Sanches <cesarsanchescv2@gmail.com>
2025-11-21 23:20:30 -0100

    corrigir index e imprimir

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { StatsProvider } from './contexts/StatsContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +13,11 @@ if (!rootElement) {
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <StatsProvider>
+        <App />
+        <SpeedInsights />
+      </StatsProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
